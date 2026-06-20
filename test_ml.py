b738baf33ddd6ccc6f36d6deb85a04aa3124dc3d
Lauren Lee <lee.lauren.m@gmail.com>
2026-06-20 14:50:01 +0000

    step4 test1-test2

--- test_ml.py
+++ test_ml.py
@@ -1,29 +1,58 @@
 import pytest
 # TODO: add necessary import
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from ml.model import compute_model_metrics
 
 # TODO: implement the first test. Change the function name and input as needed
 
-def test_one():
+def test_data_split_ratio():
     """
     # add description for the first test
-    """
+   
+    This test confirms that the data split occurs in an 80% training/ 20% testing ratio
+    """ 
     # Your code here
-    pass
+     data = pd.read_csv("data/census.csv")
+
+     train, test = train_test_split(data, test_size=0.20, random_state=42)
+
+     total_rows = len(data)
+     train_ratio = round(len(train)/total_rows,1)
+     test_ratio = round(len(test)/total_rows,1)
+
+     assert train_ratio == 0.8
+     assert test_ratio == 0.2
 
 
 # TODO: implement the second test. Change the function name and input as needed
-def test_two():
+def test_all_metrics_returned():
     """
     # add description for the second test
+
+    This test confirms that all three metrics are returned, Precision, Recall and F1 score
     """
     # Your code here
-    pass
+    y = [1,0,1,1,0,1,1,1]
+    preds = [1,1,0,0,1,1,0,0]
+
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+
+    assert 0 <= precision <= 1
+    assert 0 <= recall <= 1
+    assert 0 <= fbeta <= 1
+
+
+
+
 
 
 # TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_model_saved():
     """
     # add description for the third test
+
+    This test confims that the model is saved successfully.
     """
     # Your code here
     pass
