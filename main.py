cab75f7b5faac0f1d1267be48cd11754a980dc41
Lauren Lee <lee.lauren.m@gmail.com>
2026-06-21 14:41:33 +0000

    step4-api steps

--- main.py
+++ main.py
@@ -28,24 +28,23 @@ class Data(BaseModel):
     native_country: str = Field(..., example="United-States", alias="native-country")
 
 
-path = None  # TODO: enter the path for the saved encoder 
+path = os.path.join(os.getcwd(), "model", "encoder.pkl")  # TODO: enter the path for the saved encoder
 encoder = load_model(path)
 
 
-path = None  # TODO: enter the path for the saved model 
+path = os.path.join(os.getcwd(), "model", "model.pkl")  # TODO: enter the path for the saved model
 model = load_model(path)
 
 # TODO: create a RESTful API using FastAPI
 
-app = None  # your code here
+app = FastAPI()  # your code here
 
 # TODO: create a GET on the root giving a welcome message
 @app.get("/")
 async def get_root():
     """ Say hello!"""
     # your code here
-    pass
-
+    return {"message": "Hello World"}
 
 # TODO: create a POST on a different path that does model inference
 @app.post("/data/")
@@ -73,6 +72,10 @@ async def post_inference(data: Data):
         # use data as data input
         # use training = False
         # do not need to pass lb as input
+        data,
+        categorical_features=cat_features,
+        training =False,
+        encoder =encoder,
     )
-    _inference = None  # your code here to predict the result using data_processed
+    _inference = inference(model, data_processed)  # your code here to predict the result using data_processed
     return {"result": apply_label(_inference)}
